1cb87f61b1f2e78de9e7297c5aebe48ca987742c
zengchuqian <zengchuqian@yeah.net>
2022-03-16 19:02:21 -0400

    Update requirement

--- device/test_measurement.py
+++ device/test_measurement.py
@@ -19,20 +19,20 @@ def test_get_patient_measurement_not_exist():
     response = session.get('http://127.0.0.1:8000/device/patient-id/' + str(patient_id))
     assert response.status_code == 404
 
-def test_create_patient_measurement_success():
-    mea_data = {
-        "patient_id": 1,
-        "temperature": 36.9,
-        "systolic_blood_pressure": 101.0,
-        "diastolic_blood_pressure": 78.0,
-        "pulse": 80.0,
-        "oximeter": 96.0,
-        "weight": 60.0,
-        "glucometer": 6.0
-    }
-    session = requests.session()
-    response = session.post('http://127.0.0.1:8000/device/add/', data=mea_data)
-    assert response.status_code == 201
+# def test_create_patient_measurement_success():
+#     mea_data = {
+#         "patient_id": 1,
+#         "temperature": 36.9,
+#         "systolic_blood_pressure": 101.0,
+#         "diastolic_blood_pressure": 78.0,
+#         "pulse": 80.0,
+#         "oximeter": 96.0,
+#         "weight": 60.0,
+#         "glucometer": 6.0
+#     }
+#     session = requests.session()
+#     response = session.post('http://127.0.0.1:8000/device/add/', data=mea_data)
+#     assert response.status_code == 201
 
 def test_create_patient_not_exist_measurement():
     mea_data = {
