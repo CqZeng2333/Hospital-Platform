133e0fd0a9ccde1ac775215c8ff50729977bd4c4
zengchuqian <zengchuqian@yeah.net>
2022-03-16 17:40:41 -0400

    Remove unused packages

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
-from rest_framework.response import Response
 from . import models
 from . import serializers
+#from rest_framework.response import Response
 
 # Create your views here.
 class UserList(generics.ListAPIView):
